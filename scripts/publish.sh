420769e12f6322ea1d64c57a0a96bcba6d64458f
Sergio Rodriguez Orellana <sergio@lucidchart.com>
2019-08-15 12:40:38 -0600

    attempt with actual build

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,66 +9,45 @@
 #   exit 1
 # fi
 
-# # Maven requires a source jar and a javadoc jar to be included, but this is a Scala project
-# mkdir -p temp
-# echo "empty jar" > temp/README
-# source_jar="temp/$artifactId-$version-sources.jar"
-# javadoc_jar="temp/$artifactId-$version-javadoc.jar"
-# jar -cf "$source_jar" temp/README
-# jar -cf "$javadoc_jar" temp/README
-
-# # Determine the url to publish to based on whether this is a SNAPSHOT version
-# is_snapshot=$(echo "$version" | grep -o "SNAPSHOT" | wc -w)
-# if [[ $is_snapshot > 0 ]]; then
-# 	url="https://oss.sonatype.org/content/repositories/snapshots"
-# else
-# 	url="https://oss.sonatype.org/service/local/staging/deploy/maven2"
-# fi
-
-# # Build everything
-# #bazel clean --expunge
-# bazel build play-routes-compiler:play-routes-compiler_deploy.jar
-# bazel build play-routes-compiler:pom
-
-source_jar="to_deploy/play-routes-compiler-cli_2.12-2.7.2-sources.jar"
-javadoc_jar="to_deploy/play-routes-compiler-cli_2.12-2.7.2-javadoc.jar"
-
-deploy_jar="to_deploy/play-routes-compiler_deploy.jar"
-pom_file="to_deploy/pom.xml"
-url="https://oss.sonatype.org/service/local/staging/deploy/maven2"
-
 artifactId="play-routes-compiler-cli_2.11"
-version="2.5.19"
-groupId="com.lucidchart"
+version="test-deploy-0.1"
+
+# Maven requires a source jar and a javadoc jar to be included, but this is a Scala project
+mkdir -p temp
+echo "empty jar" > temp/README
+source_jar="temp/$artifactId-$version-sources.jar"
+javadoc_jar="temp/$artifactId-$version-javadoc.jar"
+jar -cf "$source_jar" temp/README
+jar -cf "$javadoc_jar" temp/README
+
+# Determine the url to publish to based on whether this is a SNAPSHOT version
+is_snapshot=$(echo "$version" | grep -o "SNAPSHOT" | wc -w)
+if [[ $is_snapshot > 0 ]]; then
+	url="https://oss.sonatype.org/content/repositories/snapshots"
+else
+	url="https://oss.sonatype.org/service/local/staging/deploy/maven2"
+fi
+
+# Build everything
+#bazel clean --expunge
+bazel build play-routes-compiler:play-routes-compiler_deploy.jar
+bazel build play-routes-compiler:pom
+
+deploy_jar="bazel-bin/play-routes-compiler/play-routes-compiler_deploy.jar"
+pom_file="bazel-bin/play-routes-compiler/pom.xml"
+
+# source_jar="to_deploy/play-routes-compiler-cli_2.12-2.7.2-sources.jar"
+# javadoc_jar="to_deploy/play-routes-compiler-cli_2.12-2.7.2-javadoc.jar"
+
+# deploy_jar="to_deploy/play-routes-compiler_deploy.jar"
+# pom_file="to_deploy/pom.xml"
+# url="https://oss.sonatype.org/service/local/staging/deploy/maven2"
 
 # Create signatures
 gpg -ab "$deploy_jar"
 gpg -ab "$pom_file"
 gpg -ab "$javadoc_jar"
 gpg -ab "$source_jar"
-
-# Deploy to maven
-# echo "Deploying jar.asc"
-# mvn deploy:deploy-file \
-# 	-Dfile=$jar_asc \
-# 	-DrepositoryId="oss-sonatype-org" \
-# 	-Durl="$url" \
-# 	-DartifactId="$artifactId" \
-# 	-Dversion="$version" \
-# 	-DgroupId="$groupId" \
-# 	--settings=".mvn_settings.travis.xml" \
-# 	--fail-never
-
-# echo "Deploying pom.asc"
-# mvn deploy:deploy-file \
-# 	-Dfile=$pom_asc \
-# 	-DrepositoryId="oss-sonatype-org" \
-# 	-Durl="$url" \
-# 	-DartifactId="$artifactId" \
-# 	-Dversion="$version" \
-# 	-DgroupId="$groupId" \
-# 	--settings=".mvn_settings.travis.xml" \
-# 	--fail-never
 
 echo "Deploying to Maven"
 mvn deploy:deploy-file \
